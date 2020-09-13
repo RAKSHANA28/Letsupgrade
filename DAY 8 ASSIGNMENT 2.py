cb5fcd5a8202be4b0cf8e17b8e93a8c1c528e207
RAKSHANA28 <70831442+RAKSHANA28@users.noreply.github.com>
2020-09-13 20:09:31 +0530

    Add files via upload

---
+++ DAY 8 ASSIGNMENT 2.py
@@ -0,0 +1,7 @@
+op = hello!, this is Rakshana
+ my phone no is 9765432108, 
+always motivate yourself and you can achieve anything in your life!!!
+
+import op
+from stat import S_IREAD
+op.chmod ("sample.txt" , S_IREAD)
